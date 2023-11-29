3d5ae3934daea7bee8a026d3eb073fc75cf4206a
Frank <franklaercio@gmail.com>
2023-11-29 03:14:29 -0300

    feat: init project files

---
+++ src/test/java/com/ufrn/financial/FinancialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufrn.financial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
